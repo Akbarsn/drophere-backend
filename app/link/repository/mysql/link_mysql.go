7a1fb485890c8680fbcc9324ccf6416cdbb27d25
harun ulum <student.harun0813@gmail.com>
2021-04-14 14:58:26 +0700

    insert struct inside link repository

--- app/link/repository/mysql/link_mysql.go
+++ app/link/repository/mysql/link_mysql.go
@@ -1,1 +1,38 @@
 package mysql
+
+import (
+	"github.com/bccfilkom/drophere-go/domain"
+	"github.com/jinzhu/gorm"
+)
+
+type UserRepository struct {
+	DB *gorm.DB
+}
+
+func NewUserRepository(conn *gorm.DB) domain.LinkRepository {
+	return &UserRepository{DB: conn}
+}
+
+func (u UserRepository) Create(l *domain.Link) (*domain.Link, error) {
+	panic("implement me")
+}
+
+func (u UserRepository) Delete(l *domain.Link) error {
+	panic("implement me")
+}
+
+func (u UserRepository) FindByID(id uint) (*domain.Link, error) {
+	panic("implement me")
+}
+
+func (u UserRepository) FindBySlug(slug string) (*domain.Link, error) {
+	panic("implement me")
+}
+
+func (u UserRepository) ListByUser(userID uint) ([]domain.Link, error) {
+	panic("implement me")
+}
+
+func (u UserRepository) Update(l *domain.Link) (*domain.Link, error) {
+	panic("implement me")
+}
